ba70c9ce07c85b63ce1c1f75f2b24db690104199
roza berkani <rozaberkani@gmail.com>
2024-06-26 22:18:18 +0200

    update project

---
+++ src/test/java/com/e2eTests/automation/page_objects/LoginPage.java
@@ -0,0 +1,57 @@
+package com.e2eTests.automation.page_objects;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import com.e2eTests.automation.utils.BasePage;
+import com.e2eTests.automation.utils.Setup;
+
+ 
+
+public class LoginPage extends BasePage {
+
+	/* Retrieve web element */
+	@FindBy(how = How.ID, using = "Email")
+	private static WebElement email;
+
+	@FindBy(how = How.ID, using = "Password")
+	private static WebElement password;
+
+	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
+	private static WebElement btnLogin;
+	
+	@FindBy(how = How.XPATH, using = " //body/div/div/div[1]/h1[1]") // change me
+	private static WebElement text;
+	
+	@FindBy(how = How.XPATH, using = "//a[normalize-space()='Logout']")
+	private static WebElement btnLogout;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"main\"]/div/div/div/div[1]/h1")
+	private static WebElement titlePage;
+
+	public LoginPage() {
+		super(Setup.getDriver());	
+	}
+
+	/*Create method getter */
+	public static WebElement getBtnLogout() {
+		return btnLogout;
+	}
+	
+	public static WebElement getEmail() {
+		return email;
+	}
+
+	public static WebElement getPassword() {
+		return password;
+	}
+
+	public static WebElement getBtnLogin() {
+		return btnLogin;
+	}
+
+	public static WebElement getTitlePage() {
+		return text;
+	}
+
+}
---
+++ src/test/java/com/e2eTests/automation/step_definitions/LoginStepDefinition.java
@@ -0,0 +1,64 @@
+package com.e2eTests.automation.step_definitions;
+
+ import org.junit.Assert;
+
+import com.e2eTests.automation.page_objects.LoginPage;
+import com.e2eTests.automation.utils.ConfigFileReader;
+import com.e2eTests.automation.utils.SeleniumUtils;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+
+	public LoginPage loginPage;
+	public ConfigFileReader configFileReader;
+	public SeleniumUtils seleniumUtils;
+
+	public LoginStepDefinition() {
+		loginPage = new LoginPage();
+		configFileReader = new ConfigFileReader();
+		seleniumUtils = new SeleniumUtils();
+	}
+
+	@Given("Je visite le site nopcommerce")
+	public void jeVisiteLeSiteNopcommerce() {
+		seleniumUtils.get(configFileReader.getProperties("home.url"));
+
+	}
+
+	@When("Je saisis l'adresse mail {string}")
+	public void jeSaisisLAdresseMail(String email) {
+		seleniumUtils.writeText(LoginPage.getEmail(), (configFileReader.getProperties("home.login")));
+	}
+
+	@When("Je saisis le mot de passe {string}")
+	public void jeSaisisLeMotDePasse(String password) {
+		seleniumUtils.writeText(LoginPage.getPassword(), password);
+
+	}
+
+	@When("Je clique sur le bouton login")
+	public void jeCliqueSurLeBoutonLogin() {
+		seleniumUtils.click(LoginPage.getBtnLogin());
+
+	}
+
+	@Then("Je verifie le message d'erreur {string}")
+	public void jeVerifieLeMessageDErreur(String string) {
+	}
+
+	/* TC - Logout */
+	@When("Je clique sur le bouton Logout")
+	public void jeCliqueSurLeBoutonLogout() {
+		seleniumUtils.click(LoginPage.getBtnLogout());
+	}
+
+	@Then("Je me redirige vers la page home {string}")
+	public void jeMeRedirigeVersLaPageHome(String text) {
+		  String titlePage = LoginPage.getTitlePage().getText();
+		  Assert.assertEquals(titlePage, text);
+	}
+
+}
